ef26df2edbe423e0d6e9892d1a3d3c27f1e82a3e
pratiyush159 <131248180+pratiyush159@users.noreply.github.com>
2023-07-13 21:50:07 +0530

    Add files via upload

---
+++ 3_📞Contact Us.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.header(":mailbox: Get in Touch With Me!")
+
+contact_form = """
+<form action="https://formsubmit.co/gillugillu41@gmail.com" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your Name" required>
+     <input type="email" name="email" placeholder="Your Email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+#Local CSS file
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style", unsafe_allow_html=True)
+
+local_css("style/style.css")
