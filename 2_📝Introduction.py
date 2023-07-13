6af9c301486ba4313e3362708d41f04265215a1c
pratiyush159 <131248180+pratiyush159@users.noreply.github.com>
2023-07-13 21:48:50 +0530

    Add files via upload

---
+++ 2_📝Introduction.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="SpamDetector App",
+    page_icon="👨‍💻",
+)
+
+st.title("Email Spam Detector")
+st.header("Introduction")
+st.text("Email spam refers to the unwanted and unsolicited emails that are sent to a large    ")
+st.text("number of user with the intention of advertising or promoting a product,service")
+st.text(",or website.These emails are usually sent in bulk, without the recipient's consent")
+st.text("and often contain misleading information or scams.Email spam is a significant ")
+st.text("problem that affects both individuals and businesses.It can lead to a loss of   ")
+st.text("productivity,increased security risks, and even financial losses. In recent years,   ")
+st.text("the volume of spam emails has increased exponentially,with estimates suggesting ")
+st.text("that over 50% of all emails sent worldwide are spam.")
+
+
+
+
+
+
+
+
